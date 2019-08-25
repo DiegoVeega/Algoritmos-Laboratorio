f602b3899e74185073466bd2defa110a63561650
vegadevh <vegadevh@gmail.com>
2019-08-25 16:30:16 -0600

    Ejemplo1/Laboratorio1

---
+++ Laboratorio1/FibonacciPosposicion/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: 00148816.
+ *
+ * Created on August 25, 2019, 2:51 PM
+ */
+#include <iostream>
+
+using namespace std;
+
+void fibonacci(int limite, int num1, int num2, int temp){
+    cout<<num2; //Imprime el valor de num2
+    if(num2!=limite && num2<=limite){ //Verifica si num2 no sobrepasa el limite
+        temp=num1;
+        num1=num2;
+        num2=temp+num1;
+        cout<<", ";
+        fibonacci(limite, num1, num2, temp); //Llamada  a la funcion
+    }
+};
+
+int main(int argc, char** argv) {
+    int limite, num1=0, num2=1, temp;
+    cout<<"Ingrese el numero limite para la serie: ";
+    cin>>limite;
+
+    temp=0;
+    cout<<"Serie: "<<num1<<", ";
+    fibonacci(limite, num1, num2, temp);
+    cout<<".";
+    
+    return 0;
+}
