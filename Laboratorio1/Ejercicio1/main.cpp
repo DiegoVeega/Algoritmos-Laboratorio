5bf6c3049849df6834a22c84b9399d5620bef2f9
vegadevh <vegadevh@gmail.com>
2019-08-29 18:21:02 -0600

    Base Ejercicio1 - variable con respuesta*

--- Laboratorio1/Ejercicio1/main.cpp
+++ Laboratorio1/Ejercicio1/main.cpp
@@ -6,20 +6,34 @@
 
 /* 
  * File:   main.cpp
- * Author: diego
+ * Author: 00148816.
  *
  * Created on August 29, 2019, 4:47 PM
  */
 
-#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
-/*
- * 
- */
+int SumaMatriz(int A[][3], int B[][3], int C[][3], int fila, int columna){
+    if(fila<3){
+        if(columna<3){
+            C[fila][columna]=A[fila][columna]+B[fila][columna];
+            columna++;
+            SumaMatriz(A,B,C,fila, columna);
+        }
+    }
+}
+
 int main(int argc, char** argv) {
 
+    int A[][3] = {{1,3,5},{7,9,1},{3,5,7}};
+    int B[][3] = {{9,1,3},{5,7,9},{1,3,5}};
+    int C[][3] = {{0,0,0},{0,0,0},{0,0,0}};
+    
+    int fila=0, columna=0;
+    
+    SumaMatriz(A,B,C,fila, columna);
     return 0;
 }
 
