206bfd871b25685179343e63fab6cec60096170b
vegadevh <vegadevh@gmail.com>
2019-08-29 19:44:19 -0600

    Estructura basica PILAS & .gitignore

---
+++ Laboratorio2/Tarea - CompararPilas/main.cpp
@@ -0,0 +1,81 @@
+/* 
+ * File:   main.cpp
+ * Author: 00148816.
+ *
+ * Created on August 29, 2019, 6:24 PM
+ */
+
+#include <iostream>
+
+
+using namespace std;
+
+struct Nodo{
+    int dato;
+    Nodo *sig;
+}*inicio;
+
+class Pila{
+    public:
+        Nodo* crearNodo(int valor){
+            Nodo *n =new Nodo;
+            n->dato =valor;
+            n->sig = nullptr;
+            return n;
+        }
+        
+        void push(int valor){
+            Nodo *n=crearNodo(valor);
+            n->sig = inicio;
+            inicio=n;
+        }
+        void mostrarPila(){
+            Nodo *temp = inicio;
+            if(!inicio){
+                cout<<"La pila esta vacia"<<endl;
+            }
+            else{
+                while(temp){
+                    cout<<temp->dato<<", ";
+                    temp = temp ->sig;
+                }
+            }
+        }
+};
+
+int main(int argc, char** argv) {
+    
+    //--------------------Pilas
+    Pila par;
+    Pila impar;
+    //--------------------Pilas
+    
+    int numeros[100];
+    int elementos;
+    
+    cout<<"Cuantos numeros quiere agregar? ";
+    cin>>elementos;
+    
+    for (int i = 0; i < elementos; i++) {
+        cout<<i+1<<". ";
+        cin>>numeros[i];
+    }
+
+    //Determinar si un numero es par o impar
+    for (int i = 0; i < elementos; i++) {
+        if(numeros[i]==0){
+            //Insertar en pila par
+            
+        }
+        else if(numeros[i]%2==0){
+            //Insertar en pila par
+        }
+        else{
+            //Insertar en pila impar
+        }
+    }
+
+    
+    return 0;
+}
+
