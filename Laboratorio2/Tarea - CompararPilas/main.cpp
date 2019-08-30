87fd0138add7a0cbe72d59fe39c2107eb001612c
vegadevh <vegadevh@gmail.com>
2019-08-29 20:21:22 -0600

    Solucion, funcion de dos pilas

--- Laboratorio2/Tarea - CompararPilas/main.cpp
+++ Laboratorio2/Tarea - CompararPilas/main.cpp
@@ -43,11 +43,44 @@ class Pila{
         }
 };
 
+struct NodoP{
+    int dato;
+    NodoP *sig;
+}*ini;
+
+class Pila2{
+    public:
+        NodoP* crearNodo(int valor){
+            NodoP *n =new NodoP;
+            n->dato =valor;
+            n->sig = nullptr;
+            return n;
+        }
+        
+        void push(int valor){
+            NodoP *n=crearNodo(valor);
+            n->sig = ini;
+            ini=n;
+        }
+        void mostrarPila(){
+            NodoP *temp = ini;
+            if(!ini){
+                cout<<"La pila esta vacia"<<endl;
+            }
+            else{
+                while(temp){
+                    cout<<temp->dato<<", ";
+                    temp = temp ->sig;
+                }
+            }
+        }
+};
+
 int main(int argc, char** argv) {
     
     //--------------------Pilas
     Pila par;
-    Pila impar;
+    Pila2 impar;
     //--------------------Pilas
     
     int numeros[100];
@@ -63,19 +96,6 @@ int main(int argc, char** argv) {
 
     //Determinar si un numero es par o impar
     for (int i = 0; i < elementos; i++) {
-        
-        switch(numeros[i]%2){
-            case 0:
-                cout<<numeros[i]<<"Es par";
-                par.push(numeros[i]);
-                break;
-                
-            case 1:
-                cout<<numeros[i]<<"Es impar";
-                impar.push(numeros[i]);
-                break;
-        }
-        /*
         if(numeros[i]==0){
             //Insertar en pila par
             cout<<numeros[i]<<"Es par";
@@ -92,13 +112,6 @@ int main(int argc, char** argv) {
             cout<<numeros[i]<<"Es impar";
             impar.push(numeros[i]);
         }
-        */
-        cout<<"FOR PAR";
-        par.mostrarPila();
-        cout<<"\n";
-        cout<<"FOR IMPAR";
-        impar.mostrarPila();
-        cout<<"\n";
     }
     cout<<"\n";
     cout<<"Pares: ";
